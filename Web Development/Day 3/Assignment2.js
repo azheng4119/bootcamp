e23142a2e11f6f9074476c4264392a147426d626
azheng4119 <azheng4119@bths.edu>
2019-07-17 23:37:52 -0400

    finished all functions except for last 2

--- Web Development/Day 3/Assignment2.js
+++ Web Development/Day 3/Assignment2.js
@@ -101,3 +101,130 @@ let myEvery = (arr,cb) => {
 // let isOdd = num => num%2 === 1;
 // console.log([1,2,3,4].myEvery(isOdd));
 // console.log(myEvery([1,2,3,4],isOdd));
+
+//Array.prototype.reduce
+Array.prototype.myReduce = function(cb) {
+    let sum = 0;
+    for (let i = 0; i < this.length; i++){
+        sum = cb(sum,this[i])
+    }
+    return sum;
+}
+
+//standalone function
+let myReduce = (arr,cb) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++){
+        sum = cb(sum,arr[i])
+    }
+    return sum;
+}
+
+//testing for myReduce
+// let reducer = (accumulator, currentValue) => accumulator + currentValue;
+// console.log([1,2,3,4].myReduce(reducer));
+// console.log(myReduce([1,2,3,4],reducer));
+
+//Array.prototype.includes
+Array.prototype.myIncludes = function(cb){
+    for (let i = 0; i < this.length; i++){
+         if (this[i] === cb) return true;
+    }
+    return false;
+}
+
+//standalone function
+let myIncludes = (arr,cb) => {
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] === cb) return true;
+   }
+   return false;
+}
+
+//testing myIncludes
+// console.log([1,2,3,4].myIncludes(1));
+// console.log(myIncludes([1,2,3,4],1));
+
+//Array.prototype.indexOf
+Array.prototype.myIndexOf = function(cb){
+    for (let i = 0; i < this.length; i++){
+        if (this[i] === cb) return i;
+   }
+   return -1;
+}
+
+//standalone function
+let myIndexOf = (arr,cb) => {
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] === cb) return i;
+   }
+   return -1;
+}
+
+//testif myIndexOf
+// console.log([1,2,3,4].myIndexOf(1));
+// console.log(myIndexOf([1,2,3,4],1));
+
+//Array.prototype.push
+Array.prototype.myPush = function(cb){
+    this[this.length] = cb;
+    return this.length;
+}
+
+//standalone function 
+let myPush = (arr,cb) => {
+    arr[arr.length] = cb;
+    return arr.length;
+}
+//testing for myPush
+// console.log([1,2,3,4].myPush(1));
+// console.log(myPush([1,2,3,4],1));
+
+//Array.prototype.lastIndexOf
+Array.prototype.myLastIndexOf = function(cb){
+    for (let i = this.length-1; i > -1; i--){
+        if (this[i] === cb) return i;
+    }
+    return -1;
+}
+
+//standalone function
+let myLastIndexOf = (arr,cb) => {
+    for (let i = arr.length-1; i > -1; i--){
+        if (arr[i] === cb) return i;
+    }
+    return -1;
+}
+
+//testing myLastIndexOf
+// console.log([1,2,3,4,1].myLastIndexOf(1));
+// console.log(myLastIndexOf([1,2,3,4,1],1));
+
+//Object.keys
+let getKeys = function(obj) {
+    listofkeys = [];
+    for (let x in obj){
+        listofkeys.push(x);
+    }
+    return listofkeys;
+}
+
+let object1 = {
+    a: 'somestring',
+    b: 42,
+    c: false
+  };
+
+console.log(getKeys(object1));
+
+//Objects.values
+let getValues = function(obj){
+    listofValues = [];
+    for (let x in obj){
+        listofValues.push(obj[x]);
+    }
+    return listofValues;
+}
+console.log(getValues(object1));
+
+
