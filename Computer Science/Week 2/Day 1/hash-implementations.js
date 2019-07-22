b632ffe99e2ac78522fba1a5baae5047fe3ddfe9
azheng4119 <azheng4119@bths.edu>
2019-07-21 21:42:52 -0400

    hash implementation init

---
+++ Computer Science/Week 2/Day 1/hash-implementations.js
@@ -0,0 +1,33 @@
+var MyHashSet = function() {
+    
+};
+
+MyHashSet.prototype.add = function(key) {
+    
+};
+
+MyHashSet.prototype.remove = function(key) {
+    
+};
+
+MyHashSet.prototype.contains = function(key) {
+    
+};
+
+
+
+var MyHashMap = function() {
+    
+};
+
+MyHashMap.prototype.put = function(key, value) {
+    
+};
+
+MyHashMap.prototype.get = function(key) {
+    
+};
+
+MyHashMap.prototype.remove = function(key) {
+    
+};
