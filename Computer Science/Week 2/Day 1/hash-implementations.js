4b3e0015aa29da04f6b9cb6e109731eb941deaec
azheng4119 <azheng4119@bths.edu>
2019-07-21 23:40:35 -0400

    hash assignment

--- Computer Science/Week 2/Day 1/hash-implementations.js
+++ Computer Science/Week 2/Day 1/hash-implementations.js
@@ -1,33 +1,82 @@
+/**
+ * Initialize your data structure here.
+ */
 var MyHashSet = function() {
-    
+    this.hash = [];
 };
 
+/** 
+ * @param {number} key
+ * @return {void}
+ */
 MyHashSet.prototype.add = function(key) {
-    
+    this.hash[key] = 1;
 };
 
+/** 
+ * @param {number} key
+ * @return {void}
+ */
 MyHashSet.prototype.remove = function(key) {
-    
+    delete this.hash[key];
 };
 
+/**
+ * Returns true if this set contains the specified element 
+ * @param {number} key
+ * @return {boolean}
+ */
 MyHashSet.prototype.contains = function(key) {
-    
+    return (this.hash[key] === 1);
 };
 
+/** 
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
 
-
+/**
+ * Initialize your data structure here.
+ */
 var MyHashMap = function() {
-    
+    this.hash = {};
 };
 
+/**
+ * value will always be non-negative. 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
 MyHashMap.prototype.put = function(key, value) {
-    
+    this.hash[key] = value;
 };
 
+/**
+ * Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key 
+ * @param {number} key
+ * @return {number}
+ */
 MyHashMap.prototype.get = function(key) {
-    
+    return (key in this.hash) ? this.hash[key] : -1;
 };
 
+/**
+ * Removes the mapping of the specified value key if this map contains a mapping for the key 
+ * @param {number} key
+ * @return {void}
+ */
 MyHashMap.prototype.remove = function(key) {
-    
+    delete this.hash[key];
 };
+
+/** 
+ * Your MyHashMap object will be instantiated and called as such:
+ * var obj = new MyHashMap()
+ * obj.put(key,value)
+ * var param_2 = obj.get(key)
+ * obj.remove(key)
+ */
