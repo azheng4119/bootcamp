ba08281060480620793ef91344dc5226bd9e01f8
azheng4119 <azheng4119@bths.edu>
2019-08-01 11:34:12 -0400

    reducer function for counter

--- Web Development/Week 3/Day 4/counter-redux/src/Store/utilities/counter.js
+++ Web Development/Week 3/Day 4/counter-redux/src/Store/utilities/counter.js
@@ -3,3 +3,27 @@
 //action types;
 const INCREMENT = "INCREMEMT";
 const DECREMENT = "DECREMENT";
+
+//action creaters
+const increment = () => {
+    return {
+        type: INCREMENT
+    }
+}
+
+const decrement = () => {
+    return  {
+        type: DECREMENT
+    }
+}
+
+export default (state = 0, action) => {
+    switch (action.type){
+        case INCREMENT: 
+            return state + 1;
+        case DECREMENT: 
+            return state - 1;
+        default:
+            return state;
+    }
+}
