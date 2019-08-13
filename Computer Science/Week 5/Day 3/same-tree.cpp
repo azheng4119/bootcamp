9fb6da9a7d1f0c461ce9daac1668e132f8513b9c
azheng4119 <azheng4119@bths.edu>
2019-08-12 21:14:58 -0400

    same tree

---
+++ Computer Science/Week 5/Day 3/same-tree.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+//Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  };
+ 
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        return (uniqueId(p) == uniqueId(q));
+    }
+    
+    string uniqueId(TreeNode * root) {
+        if (root == nullptr) return "null";
+        string idString = to_string(root->val);
+        idString += uniqueId(root->left) + uniqueId(root->right);
+        return idString;
+    } 
